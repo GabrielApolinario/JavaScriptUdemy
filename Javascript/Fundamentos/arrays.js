767253a32822382bc8a7ee7e407f0981f9e9e8cd
Gabriel Silva <gsilva@mstrategy.local>
2019-12-17 15:49:21 -0300

    Upando projeto

---
+++ Javascript/Fundamentos/arrays.js
@@ -0,0 +1,12 @@
+//JS aceita todos os tipos de valores dentro de um mesmo array, sendo possível mistura-los
+
+const valores = [1, 5.2, 7.1, 8.3]
+console.log(valores[0],valores [3]) //Imprimindo a primeira posição e a terceira
+
+console.log(valores.length) //Quantidade de posições dentro do array
+
+valores.push(3, 7) //Adiciona valores ao final do array
+console.log(valores)
+
+valores.pop() //Deleta a última posição do array
+console.log(valores)
