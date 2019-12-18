422bf3199fcafba0f34403db54560b70cb18840e
Gabriel Silva <gsilva@mstrategy.local>
2019-12-18 13:30:23 -0300

    Alteração objeto.js

--- Javascript/Fundamentos/objeto.js
+++ Javascript/Fundamentos/objeto.js
@@ -3,3 +3,11 @@ Objeto1.nome = "Nome do objeto" //Criação de atributos
 Objeto1.tamanho = 25
 
 console.log(Objeto1)
+
+//Outra forma de criar objetos e passar atributos
+const Objeto2 = { 
+    nome: "Nome do objeto",
+    tamanho: 10,
+}
+
+console.log(Objeto2)
